9d47e17e4351b7cc7d7d51899324aad12bad7772
adolfmacro <m4nikamran@gmail.com>
2022-04-18 00:09:50 +0430

    add updater

--- eyeRat.py
+++ eyeRat.py
@@ -1,9 +1,12 @@
-from tools import generator
+from importlib_metadata import version
+from tools import generator,updater
 from colorama import Back , Fore
 from tools import launcher
 from os import system
 from platform import system as osType 
 from subprocess import getoutput
+from os.path import isfile
+from requests import get
 def chdps():
   QModules=['opencv','numpy','pyaudio','pyautogui','cryptography','pymsgbox','pillow','psutil','colorama']
   ntInstall=[]
@@ -41,11 +44,11 @@ def main():
     ____)_/\_(____`.  `-._______________________________________.'/         
    (___._/  \_.___) `-.________________________________________.-'          
 
-                 {Fore.RED}[ {Fore.CYAN}1 {Fore.RED}]{Fore.CYAN}Client launcher{Fore.RED}    [ {Fore.CYAN}2 {Fore.RED}] {Fore.CYAN}Generate normal connection rat
+                 {Fore.RED}[ {Fore.CYAN}1 {Fore.RED}]{Fore.CYAN}Client launcher{Fore.RED}              [ {Fore.CYAN}2 {Fore.RED}] {Fore.CYAN}Generate normal connection rat
                                                                                                    
-                 {Fore.RED}[ {Fore.CYAN}3 {Fore.RED}]{Fore.CYAN}Server launcher{Fore.RED}    [ {Fore.CYAN}4 {Fore.RED}] {Fore.CYAN}Generate reverse connection rat 
+                 {Fore.RED}[ {Fore.CYAN}3 {Fore.RED}]{Fore.CYAN}Server launcher{Fore.RED}              [ {Fore.CYAN}4 {Fore.RED}] {Fore.CYAN}Generate reverse connection rat 
 
-                 {Fore.RED}[ {Fore.CYAN}5 {Fore.RED}] {Fore.CYAN}Developer information
+                 {Fore.RED}[ {Fore.CYAN}5 {Fore.RED}] {Fore.CYAN}Developer information{Fore.RED}       [ {Fore.CYAN}6 {Fore.RED}] {Fore.CYAN}Check for updates
                   
                   {Fore.LIGHTGREEN_EX}Enter your selection :{Fore.MAGENTA} """)
   clear()
@@ -84,6 +87,8 @@ def main():
     
     
     Enter to exit : """)
+  elif selection=="6":
+    updater.mainUpdater()
 if __name__=='__main__':
   chdps()
   while 1:
